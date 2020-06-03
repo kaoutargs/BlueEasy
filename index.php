d8bccfb504fb670f86cfd43a5f417fad8e746bac
kaoutargs <65283017+kaoutargs@users.noreply.github.com>
2020-06-03 11:56:01 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,353 @@
-<?php include('header.php');?>
+<!DOCTYPE html>
+<html lang-"fr">
 
+<head>
+<title>BlueEasy</title>
+
+  <meta charset="utf-8">
+ <link rel="icon" type="image/jpg" href="img/logo.jpg" />  <meta name="viewport" content="width=device-width, initial-scale=1 shrink-to-fit=no">
+
+   <!-- BOOTSTRAP CSS-->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+  <link rel="stylesheet" href="main.css">
+</head>
+
+<body>
+  <header>
 
 <!-- Navbar -->
 
+  <nav class="navbar navbar-expand-lg navbar-dark text-white navopacity fixed-top">
+    <div class="container">
+      <a class="navbar-brand text-uppercase font-weight-bold margin-right-navbar"  href="#">blu<span class="blue">e</span>asy</a>
+
+          <div class="d-flex justify-content-around">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false"
+                            aria-label="Toggle navigation">
+                      <span class="navbar-toggler-icon"></span>
+                    </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+          <div class="navbar-nav font-weight-bold">
+          <a class="px-4 nav item nav-link active font-weight-bold" href="#">HOME</a>
+          <a class="px-4 nav item nav-link active font-weight-bold"  href="#services">SERVICES</a>
+          <a class="px-4 nav item nav-link active font-weight-bold" href="#about">FEATURES</a>
+          <a class="px-4 nav item nav-link active font-weight-bold" href="#portfolio">PORTFOLIO</a>
+          <a class="px-4 nav item nav-link active font-weight-bold" href="#contact">CONTACT</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
+<!-- Fin Navbar -->
+
+<div class="container d-flex align-items-center justify-content-center h-100">
+  <div class="row d-flex align-items-center justify-content-center">
+    <div class="col-lg-10">
+      <div class="row d-flex justify-content-center">
+        <div class="col-lg-9">
+          <h2 class="py-2 text-uppercase text-white bg-dark text-center font-weight-lighter"<span style="white-space: nowrap">"I'm looking for the unexpected.</h2></span>
+            </div>
+          </div>
+          <h2 class="py-2 text-uppercase text-white bg-dark text-center font-weight-lighter"<span style="white-space: nowrap">I'm looking for things i've never seen before."</h2></span>
+        </div>
+      </div>
+    </div>
+</header>
+<main>
+
+<!-- Services -->
+
+<section id="services">
+<div class="bg d-flex justify-content-center">
+    <div class="container d-flex justify-content-center h-100 flex-column">
+      <div class="row">
+        <div class="col-lg-12">
+           <h2 class="services text-white font-weight-lighter">SERVICES</h2>
+           <hr class="bg-white d-inline-block text-left mt-0">
+         </div>
+       </div>
+     <div class="row mt-4">
+
+       <div class="service-text col-lg-3">
+         <img src="img/coffee.png" alt="coffee" class="img-fluid mb-3" height="48px" width="34px"/>
+         <h4>COFFEE</h4>
+        <p>Apple pie icing sweet. Brownie jelly-o applicake applicake sweet roll liquorice bear claw. Jujubes carrot cake cotton candy sweet tart brownie. Tiramisu applicake jujubes.</p>
+       </div>
+
+       <div class="service-text col-lg-3">
+         <img src="img/instant.png" alt="camera" class="img-fluid mb-3"  height="48px" width="48px"/>
+         <h4>INSTANT</h4>
+         <p>Apple pie icing sweet. Brownie jelly-o applicake applicake sweet roll liquorice bear claw. Jujubes carrot cake cotton candy sweet tart brownie. Tiramisu applicake jujubes.</p>
+       </div>
+
+       <div class="service-text col-lg-3">
+         <img src="img/dslr.png" alt="dslr" class="img-fluid mb-3"  height="47px" width="45px"/>
+         <h4>SERIOUS</h4>
+         <p>Apple pie icing sweet. Brownie jelly-o applicake applicake sweet roll liquorice bear claw. Jujubes carrot cake cotton candy sweet tart brownie. Tiramisu applicake jujubes.</p>
+       </div>
+
+       <div class="service-text col-lg-3">
+         <img src="img/frame.png" alt="photo frame" class="img-fluid mb-3" height="48px" width="48px"/>
+         <h4>FRAME</h4>
+         <p>Apple pie icing sweet. Brownie jelly-o applicake applicake sweet roll liquorice bear claw. Jujubes carrot cake cotton candy sweet tart brownie. Tiramisu applicake jujubes.</p>
+       </div>
+     </div>
+    </div>
+  </div>
+</section>
+
+<!-- PORTFOLIO -->
+
+<section id="portfolio">
+<div class="bg3 d-flex justify-content-center">
+  <div class="container d-flex justify-content-center h-100 flex-column text-secondary">
+    <div class="row">
+
+    <div class="col-lg-3 pt-2">
+      <h2 class="font-weight-light">PORTFOLIO</h2>
+      <hr class="bg-secondary d-inline-block text-left mt-0 ">
+    </div>
+
+    <div class="col-lg-9 d-flex justify-content-end">
+      <nav class="navbar navbar-expand-lg navbar-light p-0">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#portfolio-nav" aria-controls="portfolio-nav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="portfolio-nav">
+          <div class="navbar-nav font-weight-bold">
+            <a class="px-4 nav-item nav-link active" href="#portfolio">ALL</a>
+            <a class="px-4 nav-item nav-link active" href="#portfolio">PANORAMAS</a>
+            <a class="px-4 nav-item nav-link active" href="#portfolio">PORTRAITS</a>
+            <a class="px-4 nav-item nav-link active" href="#portfolio">MACRO</a>
+            <a class="nav-item nav-link active" href="#portfolio">JOURNAL</a>
+          </div>
+        </div>
+      </nav>
+    </div>
+  </div>
 
 
+  <div class="row pt-5">
+
+          <div class="col-md-3 mb-4 pictures cont">
+            <img src="img/pic1.jpg" alt="Pont" class="img-fluid ii">
+            <div class="overlay bg-dark-opacity d-flex justify-content-center align-items-center"> <img src="img/plus.png" alt="icon plus" class="pic"></div>
+          </div>
+
+          <div class="col-md-3 mb-4 pictures cont">
+              <img src="img/pic2.jpg" alt="Pont-de-San-Francisco" class="img-fluid ii">
+              <div class="overlay bg-dark-opacity d-flex justify-content-center align-items-center"> <img src="img/plus.png" alt="icon plus" class="pic"></div>
+          </div>
+
+          <div class="col-md-3 mb-4 pictures cont">
+              <img src="img/pic3.jpg" alt="Paysage-mer" class="img-fluid ii">
+              <div class="overlay bg-dark-opacity d-flex justify-content-center align-items-center"> <img src="img/plus.png" alt="icon plus" class="pic"></div>
+          </div>
+
+          <div class="col-md-3 mb-4 pictures cont">
+              <img src="img/pic4.jpg" alt="Paysage-lac" class="img-fluid ii">
+              <div class="overlay bg-dark-opacity d-flex justify-content-center align-items-center"> <img src="img/plus.png" alt="icon plus" class="pic"></div>
+          </div>
+
+          <div class="col-md-3 mb-4 pictures cont">
+              <img src="img/pic5.jpg" alt="Cabane-en-bois" class="img-fluid ii">
+              <div class="overlay bg-dark-opacity d-flex justify-content-center align-items-center"> <img src="img/plus.png" alt="icon plus" class="pic"></div>
+          </div>
+
+          <div class="col-md-3 mb-4 pictures cont">
+              <img src="img/pic6.jpg" alt="Surfeur" class="img-fluid ii">
+              <div class="overlay bg-dark-opacity d-flex justify-content-center align-items-center"> <img src="img/plus.png" alt="icon plus" class="pic"></div>
+          </div>
+
+          <div class="col-md-3 mb-4 pictures cont">
+              <img src="img/pic7.jpg" alt="Montagne-eneigée" class="img-fluid ii">
+              <div class="overlay bg-dark-opacity d-flex justify-content-center align-items-center"> <img src="img/plus.png" alt="icon plus" class="pic"></div>
+          </div>
+
+          <div class="col-md-3 mb-4 pictures cont">
+              <img src="img/pic8.jpg" alt="Montagne-eneigée" class="img-fluid ii">
+              <div class="overlay bg-dark-opacitd-flex d-flex justify-content-center align-items-center"> <img src="img/plus.png" alt="icon plus" class="pic"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+</section>
 
 
+<!-- VIMEO -->
 
-<?php include('footer.php');
+<section id="vimeo">
+  <div class="container d-flex align-items-center justify-content-center h-100">
+    <div class="row">
 
- ?>
+      <div class="col-lg-5">
+        <h2 class="text-white font-weight-lighter">JUST DEFAULT SECTION</h2>
+        <hr class="bg-white d-inline-block text-left mt-0 mb-4">
+        <p class="mb-4 pr-3 text-white">Bear claw marzipan bear claw applicake I love muffin. Lemon drops gummi bears pastry gummi bears sesame snaps I love unerdwear.com.
+          Soufflé cotton candy dessert candy ice cream wafer gummies cheesecake brownie.</p>
+        <p class="pr-3 text-white">Muffin chupa chups jelly beans sweet pie applicake. Croissant chocolate cake I love pudding.
+          Ice cream I love powder pudding apple pie marshmallow. Cupcake marzipan oat cake bonbon I love candy canes toffee.</p>
+        <div class="col-lg-5 pl-0">
+          <button type="button" class="btn btn-outline-dark btn-block font-weight-bold rounded-0">VISIT ME</button>
+        </div>
+      </div>
+
+      <div class="offset-1 col-lg-6">
+        <div class="embed-responsive embed-responsive-16by9">
+<iframe width="560" height="315" src="https://www.youtube.com/embed/UQCuT1cIXp8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!-- TWITTER -->
+
+<section id="twitter">
+  <div class="container d-flex justify-content-center h-100 flex-column">
+    <div class="row">
+
+      <div class="col-lg-3">
+        <h2 class="font-weight-light text-white">TWITTER</h2>
+        <hr class="bg-white d-inline-block text-left mt-0">
+      </div>
+
+      <div class="col-lg-9 d-flex justify-content-end">
+          <h4><img src="img/twitter.png" class="img-fluid p-2" alt="icone twitter"></h4>
+      </div>
+
+      <div class="col-lg-11 mt-2">
+        <h4 class="font-weight-lighter text-white">Unerdwear cookie liquorice. Cake donut cupcake lollipop soufflé candy.
+          Chocolate oat cake <span class="font-weight-bold">@cheesecake</span> tootsie roll.</h4>
+      </div>
+
+    </div>
+  </div>
+</div>
+</section>
+
+
+<!-- WHO IS -->
+
+<section id="about">
+    <div class="container d-flex flex-column justify-content-center align-items-start h-100">
+      <div class="row">
+        <div class="col-lg-12">
+          <h2 class="font-weight-lighter text-white">WHO IS JOHN DOE?</h2>
+          <hr class="bg-white d-inline-block text-left mt-0 mb-4">
+        </div>
+      </div>
+
+      <div class="row">
+
+        <div class="col-lg-6 text-white">
+        <p>Bear claw marzipan bear claw applicake I love muffin. Lemon drops gummi bears pastry gummi bears sesame snaps I love unerdwear.com.
+          Soufflé cotton candy dessert candy ice cream wafer gummies cheesecake brownie.</p>
+        <p>Muffin chupa chups jelly beans sweet pie applicake. Croissant chocolate cake I love pudding.
+          Ice cream I love powder pudding apple pie marshmallow. Cupcake marzipan oat cake bonbon I love candy canes toffee.</p>
+        </div>
+
+        <div class="col-lg-6 text-white">
+        <p>Pudding dessert jujubes tiramisu gingerbread croissant tiramisu applicake.
+          Sesame snaps sugar plum cotton candy chocolate bonbon lemon drops candy canes cotton candy. Cake toffee pie bear claw pastry.</p>
+
+          <div class="row pl-2 pt-2">
+            <h4 class=" mx-2 d-flex justify-content-center align-items-center" href="#">
+          <a href="#about">  <img src="img/twitter.png" class="img-fluid p-2" alt="twitter"></h4></a>
+
+            <h4 class=" mx-2 d-flex justify-content-center align-items-center">
+          <a href="#about">    <img src="img/gmail.png" class="img-fluid p-2" alt="gmail+"></h4></a>
+
+            <h4 class=" mx-2 d-flex justify-content-center align-items-center">
+          <a href="#about">    <img src="img/dribbble.png" class="img-fluid p-2" alt="dribbble"></h4></a>
+
+            <h4 class=" mx-2 d-flex justify-content-center align-items-center">
+        <a href="#about">      <img src="img/icon-coffee.png" class="img-fluid p-2" alt="coffee"></h4></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+<!-- TESTIMONIALS -->
+
+<section id="testimonials">
+  <div class="container d-flex justify-content-center h-100 flex-column text-secondary">
+    <div class="row">
+
+      <div class="col-lg-3">
+        <h2 class="font-weight-lighter">TESTIMONIALS</h2>
+        <hr class="bg-secondary d-inline-block text-left mt-0 ">
+      </div>
+
+      <div class="col-lg-9 d-flex justify-content-end">
+          <h4><img src="img/quotation.png" class="img-fluid p-2" alt="icon quotation"></h4>
+      </div>
+
+      <div class="col-lg-11 mt-2">
+        <h4 class="font-weight-lighter">Applicake chocolate cake wafer toffee pie soufflé wafer.
+          Tart marshmallow wafer macaroon cheesecake jelly. Gingerbread cookie soufflé sweet roll sweet roll jelly-o.</h4>
+          <p class="mt-5 blue font-weight-bold">-Walter White</p>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!-- CONTACT -->
+
+<section id="contact" class="text-white">
+  <div class="container d-flex justify-content-center h-100 flex-column">
+    <div class="row">
+
+      <div class="col-lg-12">
+        <h2 class="font-weight-light">CONTACT</h2>
+        <hr class="bg-white d-inline-block text-left mt-0 mb-4">
+      </div>
+
+      <div class="col-lg-5">
+        <label for="name" class="font-weight-bold">FULL NAME</label>
+        <input type="text" class="form-control border-white bg-transparent rounded-0 form-control-lg" id="name">
+      </div>
+
+      <div class="offset-1 col-lg-5 pt-3">
+        <p class="font-weight-semi-bold">Jujubes bear claw croissant wafer sweet lemon drops brownie chocolate bar. Sweet lollipop halvah tootsie roll cake fruitcake.
+          Chocolate cake chocolate bar gingerbread lollipop tart. Jujubes bear claw donut chupa chups.</p>
+      </div>
+
+      <div class="col-lg-5">
+        <label for="email" class="font-weight-bold">EMAIL</label>
+        <input type="text" class="form-control border-white bg-transparent rounded-0 form-control-lg" id="email">
+      </div>
+
+      <div class="offset-1 col-lg-5 pt-3">
+        <p class="font-weight-semi-bold">Linda Newman<br/>
+          W325 State Road 123<br/>
+          Mondovi, WI(Wisconsin) 98746-54321<br/>
+          (715) 946 - 1234</p>
+      </div>
+
+    <div class="col-lg-12">
+      <label for="message" class="font-weight-bold">MESSAGE</label>
+      <textarea class="form-control border-white bg-transparent rounded-0" id="message"></textarea>
+    </div>
+
+    <div class="col-lg-4 mt-5">
+      <button type="button" class="p-3 btn-block btn btn-sm text-white font-weight-bold rounded-0">MESSAGE</button>
+    </div>
+
+    </div>
+  </div>
+</section>
+
+</main>
+
+<?php include 'footer.php' ?>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+<script src=""
+
+</body>
+</html>
+
