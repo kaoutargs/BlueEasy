177cccf93e270daf793d4bf97579d984e2894aa9
kaoutargs <65283017+kaoutargs@users.noreply.github.com>
2020-06-03 11:56:24 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,19 +1,27 @@
-<footer class="page-footer font-small pt-4 d-flex align-items-center text-center" id="footer">
+<footer class="bg6">
+  <div class="container d-flex justify-content-center align-items-around h-100 flex-column">
+    <div class="row">
 
+      <div class="col-lg-10">
+        <ul class="nav">
+          <li class="nav-item">
+            <a class="nav-link text-secondary p-0 mx-2 font-weight-bold" href="#header">HOME</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-secondary p-0 mx-2 font-weight-bold" href="#services">SERVICES</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-secondary p-0 mx-2 font-weight-bold" href="#portfolio">PORTFOLIO</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-secondary p-0 mx-2 font-weight-bold" href="#contact">CONTACT</a>
+          </li>
+        </ul>
+      </div>
 
+      <div class="col-lg-2 text-secondary font-weight-bold">
+          <p>2013 ©</p>
+      </div>
+    </div>
+  </div>
 </footer>
-
-
-
-
-
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
-
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.min.js"></script>
-
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.18.0/js/mdb.min.js"></script>
-
-</body>
-</html>
